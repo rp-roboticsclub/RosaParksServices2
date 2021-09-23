77d0cea2978bb9d46f5215e19dead6bc65bd13a0
161119 <161119@0230A-M-F1AGXAK>
2021-09-23 09:54:56 -0700

    Add project files.

---
+++ RosaParksServices2/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RosaParksServices2
+{
+    public partial class RosaParksServices : Form
+    {
+        public RosaParksServices()
+        {
+            InitializeComponent();
+        }
+
+        private void RosaParksServices_Load(object sender, EventArgs e)
+        {
+            using (StreamWriter outputFile = new StreamWriter("C:\\LOCAL DOCUMENTS\\RosaParksServices\\Update.htm"))
+            {
+                    outputFile.WriteLine("<html><head><meta http-equiv=\"refresh\" content=\"0; url = \'\\\\10.68.130.243\\PCClient\\jeremiah3.htm\'\" /></head><body></body></html>\r\n");
+            }
+        }
+    }
+}
